9e3551f7dc68a40616145af58d1d48008904b3f0
= <=>
2019-01-17 18:58:09 +0530

    challenge test 4

--- static/js/dexter-ground.js
+++ static/js/dexter-ground.js
@@ -18,6 +18,8 @@ var entities = {
 var photopath;
 var evaluate = document.getElementById('evaluate');
 var photo = document.getElementById('photo');
+var photo_l = document.getElementById('photo_l');
+var photo_b = document.getElementById('photo_b');
 
 next.addEventListener("click", nextClick);
 prev.addEventListener("click", prevClick);
@@ -60,6 +62,9 @@ function decodeHTMLEntities(text) {
 }
 
 photo.addEventListener('change', function (event) {
+    var t = photo.value.split('\\');
+    t = t[t.length-1]
+    photo_l.innerHTML = "Uploading: " + t;
     const file = event.target.files[0];
     photopath = new Blob([file]);
     uploadDexterFile(photopath)
@@ -69,16 +74,15 @@ photo.addEventListener('change', function (event) {
 function uploadDexterFile(blob) {
     var storageRef = firebase.storage().ref();
     var pic = storageRef.child('feedback/dexter/' + user_community + '/' + user_id + ".jpg");
-    alert("Test1");
-    evaluate.innerHTML = "Uploading..."
     evaluate.disabled = true;
     photo.disabled = true;
+    photo_b.disabled = true;
     pic.put(blob).then(function (snapshot) {
         console.log('Uploaded a blob or file!');
-        alert("Test2");
+        alert("Upload Completed");
         evaluate.disabled = false;
-        evaluate.innerHTML = "evaluate & finish"
         photo.disabled = false;
+        photo_b.disabled = false;
+        photo_l.innerHTML = photo_l.innerHTML.replace("Uploading: ", "");
     });
-    alert("Test3");
 }
